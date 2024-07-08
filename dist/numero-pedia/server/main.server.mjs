9b2ebac71a995b23487a5281ffb89dc00c93f025
Chandan Gowda B S <g-chandan@inahosolutions.com>
2024-07-08 16:34:20 +0530

    adding build

--- dist/numero-pedia/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-XX4QHDZ3.mjs";import"./chunk-CCFDCAVW.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ dist/numeropedia/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2GDHHJKI.mjs";import"./chunk-CCFDCAVW.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
